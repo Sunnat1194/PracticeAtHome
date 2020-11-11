59974f55948bbb6b7a95e99d9f90eba72c965a0d
day1 <Sunnat1194@gmail.com>
2020-11-11 12:23:21 -0500

    Add new project about my idea

---
+++ src/Practice2.java
@@ -0,0 +1,7 @@
+public class Practice2 {
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub");
+        System.out.println("I don't know what I should say");
+        System.out.println("I like Cadillac then Yukon");
+    }
+}
